65c72e61f091ced9b2ad70d403f4b5ba5eeda190
Tim Jedrek <tim@timjedrek.com>
2024-01-16 18:34:41 -0600

    start on homepage skeleton

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,17 @@ export default {
 			colors: {
         'main-red': '#993333',
       },
+      backgroundImage : {
+        //'CLASS' : "url('URL-TO-IMAGE')",
+      },
+      height: theme => ({
+        'screen-1/2': '50vh',
+        'screen-2/3': '66vh',
+        'screen-1/3': 'calc(100vh / 3)',
+        'screen-3/4': '75vh',
+        'screen-4/5': '80vh',
+        'screen-mobile' : '92vh', //depends on menu height
+      }),
 		},
 	},
 	plugins: [],
