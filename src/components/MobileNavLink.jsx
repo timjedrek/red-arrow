5bf5dafe7f9cb0c0bc8d7e0bc69ea0749c2a76f7
Diego <diegoyon@outlook.com>
2024-01-19 09:56:59 -0600

    show if the route is active

--- src/components/MobileNavLink.jsx
+++ src/components/MobileNavLink.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import { FaPlus, FaMinus } from "react-icons/fa6";
 
-const MobileNavLink = ({menuItem}) => {
+const MobileNavLink = ({menuItem, pathname}) => {
   const [toggled, setToggled] = useState(false);
+  const isActive = menuItem.submenu.some((item) => item.link === pathname);
 
   const handleClick = () => {
     setToggled(!toggled)
@@ -10,16 +11,15 @@ const MobileNavLink = ({menuItem}) => {
 
   return (
     <div className="flex flex-col items-start">
-      <button onClick={handleClick} className="text-xl w-full flex items-center justify-between text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap">
+      <button onClick={handleClick} className={`text-xl w-full flex items-center justify-between text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap ${isActive ? "border-b-2" : ""}`}>
         <p>{menuItem.name}</p>
-        {toggled ? <FaMinus /> : <FaPlus />}
+        {toggled ? <FaMinus className="mr-3" /> : <FaPlus className="mr-3" />}
       </button>
-      <ul className={`overflow-hidden text-white ${toggled ? 'max-h-96' : 'max-h-0'} transition-[max-height] duration-300 ease-in-out`}>
+      <ul className={`overflow-hidden text-white ${toggled ? 'max-h-96' : 'max-h-0'} transition-[max-height] duration-300 ease-in-out }`}>
       {menuItem.submenu.map((item) => (
-        <li key={item.name} className="m-4"><a href={item.link} className="hover:border-b-2 border-main-red">{item.name}</a></li>
+        <li key={item.name} className="m-4"><a href={item.link} className={`hover:border-b-2 border-main-red ${item.link === pathname ? "border-b-2" : ""}`}>{item.name}</a></li>
       ))}
       </ul>
-      
     </div>
   )
 }
