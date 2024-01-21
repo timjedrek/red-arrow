6f201566cfa9f2be64a85d052fa209ffcac693ed
Diego <diegoyon@outlook.com>
2024-01-21 12:07:58 -0600

    add prettier extension

--- src/components/MobileNavLink.jsx
+++ src/components/MobileNavLink.jsx
@@ -1,25 +1,44 @@
-import { useState } from 'react'
 import { FaPlus, FaMinus } from "react-icons/fa6";
 
-const MobileNavLink = ({menuItem, pathname, toggled, onShow}) => {
-  // const [toggled, setToggled] = useState(false);
+const MobileNavLink = ({ menuItem, pathname, toggled, onShow }) => {
   const isActive = menuItem.submenu.some((item) => item.link === pathname);
 
   return (
     <div className="flex flex-col items-start">
-      <button id={menuItem.name} onClick={onShow} className={`text-xl w-full flex items-center justify-between text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap ${isActive ? "border-b-2" : ""}`}>
+      <button
+        id={menuItem.name}
+        onClick={onShow}
+        className={`text-xl w-full flex items-center justify-between text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap ${
+          isActive ? "border-b-2" : ""
+        }`}
+      >
         <p>{menuItem.name}</p>
-        {toggled ? <FaMinus className="mr-3 pointer-events-none" /> : <FaPlus className="mr-3 pointer-events-none" />}
+        {toggled ? (
+          <FaMinus className="mr-3 pointer-events-none" />
+        ) : (
+          <FaPlus className="mr-3 pointer-events-none" />
+        )}
       </button>
-      <ul className={`overflow-hidden text-white ${toggled ? 'max-h-96' : 'max-h-0'} transition-[max-height] duration-300 ease-in-out }`}>
-      {menuItem.submenu.map((item) => (
-        <li key={item.name} className="m-4"><a href={item.link} className={`hover:border-b-2 border-main-red ${item.link === pathname ? "border-b-2" : ""}`}>{item.name}</a></li>
-      ))}
+      <ul
+        className={`overflow-hidden text-white ${
+          toggled ? "max-h-96" : "max-h-0"
+        } transition-[max-height] duration-300 ease-in-out }`}
+      >
+        {menuItem.submenu.map((item) => (
+          <li key={item.name} className="m-4">
+            <a
+              href={item.link}
+              className={`hover:border-b-2 border-main-red ${
+                item.link === pathname ? "border-b-2" : ""
+              }`}
+            >
+              {item.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default MobileNavLink
-
-
+export default MobileNavLink;
