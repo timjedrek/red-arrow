cef5bc2a3cc1cd5199651d77f115c4e6eed8fd78
Diego <diegoyon@outlook.com>
2024-01-21 12:07:58 -0600

    add prettier extension

--- src/components/MobileNavBar.jsx
+++ src/components/MobileNavBar.jsx
@@ -1,25 +1,39 @@
 import { flightPrograms, resources, about } from "../consts.ts";
-import MobileNavLink from './MobileNavLink.jsx'
-import { useState } from 'react';
+import MobileNavLink from "./MobileNavLink.jsx";
+import { useState } from "react";
 
-const MobileNavBar = ({pathname}) => {
-  const [active, setActive] = useState("");
+const MobileNavBar = ({ pathname }) => {
+  const [open, setOpen] = useState("");
   const handleClick = (e) => {
-    if (active === e.target.id) {
-      setActive("");
+    if (open === e.target.id) {
+      setOpen("");
+    } else {
+      setOpen(e.target.id);
     }
-    else {
-      setActive(e.target.id);
-    }
-  }
+  };
 
   return (
     <>
-      <MobileNavLink menuItem={flightPrograms} pathname={pathname} toggled={flightPrograms.name === active} onShow={handleClick}  />
-      <MobileNavLink menuItem={resources} pathname={pathname} toggled={resources.name === active} onShow={handleClick} />
-      <MobileNavLink menuItem={about} pathname={pathname} toggled={about.name === active} onShow={handleClick} />
+      <MobileNavLink
+        menuItem={flightPrograms}
+        pathname={pathname}
+        toggled={flightPrograms.name === open}
+        onShow={handleClick}
+      />
+      <MobileNavLink
+        menuItem={resources}
+        pathname={pathname}
+        toggled={resources.name === open}
+        onShow={handleClick}
+      />
+      <MobileNavLink
+        menuItem={about}
+        pathname={pathname}
+        toggled={about.name === open}
+        onShow={handleClick}
+      />
     </>
-  )
-}
+  );
+};
 
-export default MobileNavBar
+export default MobileNavBar;
