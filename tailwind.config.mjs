4c60b4673d73d085f2229b92d475ece353cbfacf
Diego <diegoyon@outlook.com>
2024-01-22 16:20:08 -0600

    add training courses page with homepage skeleton

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,33 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-        'main-red': '#993333',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "main-red": "#993333",
       },
-      backgroundImage : {
-        'home' : "url('/img/cessna-pilot-center-el-paso-tx.webp')",
-        'two-cessna' : "url('/img/two-cessna-red-arrow-KDNA-santa-teresa.webp')",
+      backgroundImage: {
+        home: "url('/img/cessna-pilot-center-el-paso-tx.webp')",
+        "two-cessna": "url('/img/two-cessna-red-arrow-KDNA-santa-teresa.webp')",
+        "training-courses": "url('/img/training-courses-banner.webp')",
       },
-      height: theme => ({
-        'screen-1/2': '50vh',
-        'screen-2/3': '66vh',
-        'screen-1/3': 'calc(100vh / 3)',
-        'screen-3/4': '75vh',
-        'screen-4/5': '80vh',
-        'screen-mobile' : '92vh', //depends on menu height
+      height: (theme) => ({
+        "screen-1/2": "50vh",
+        "screen-2/3": "66vh",
+        "screen-1/3": "calc(100vh / 3)",
+        "screen-3/4": "75vh",
+        "screen-4/5": "80vh",
+        "screen-mobile": "92vh", //depends on menu height
       }),
       keyframes: {
         wiggle: {
-          '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' },
-        }
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
       },
       animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-      }
-		},
-	},
-	plugins: [],
-}
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+    },
+  },
+  plugins: [],
+};
