cda27ccf4160bbece4e347bae2d9c88e46c2fac2
Diego <diegoyon@outlook.com>
2024-01-21 23:49:03 -0600

    refactor code to react components and add some styles to desktop navbar

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,23 +1,31 @@
-const NavLink = ({ object }) => {
+const NavLink = ({ menuItem, pathname, toggled, onShow }) => {
+  const isActive = menuItem.submenu.some((item) => item.link === pathname);
   return (
-    <div class="relative">
-      <a
-        class={`${object.className} nav-link cursor-pointer text-xl text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap`}
-        data-link-container={`${object.className}-links`}
+    <div className="relative">
+      <button
+        id={menuItem.name}
+        onClick={onShow}
+        className={`text-xl w-full flex items-center justify-between text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap ${
+          isActive ? "border-b-2" : ""
+        }`}
       >
-        {object.name}
-      </a>
-      <ul
-        class={`${object.className}-links links-container absolute bg-white hidden p-5 flex-col gap-2 w-60 top-12`}
+        {menuItem.name}
+      </button>
+      <div
+        className={`overflow-hidden text-white ${
+          toggled ? "max-h-96" : "max-h-0"
+        } transition-[max-height] duration-300 ease-in-out absolute bg-black flex flex-col whitespace-nowrap`}
       >
-        {object.submenu.map((item) => (
-          <div>
-            <a href={item.link} class="hover:border-b-2 border-main-red">
-              {item.name}
-            </a>
-          </div>
+        {menuItem.submenu.map((item) => (
+          <a
+            key={item.name}
+            href={item.link}
+            className={`${item.link === pathname ? "bg-main-red" : ""} px-5 py-3 border border-gray-700 hover:bg-main-red`}
+          >
+            {item.name}
+          </a>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,82 +4,107 @@ import { flightPrograms, resources, about } from "../consts.ts";
 import { useState } from "react";
 
 const Navbar = ({ pathname }) => {
-  const [open, setOpen] = useState(false);
+  const [openMobile, setOpenMobile] = useState(false);
+  const [openSubmenu, setOpenSubmenu] = useState("");
 
-  const handleClick = () => {
-    setOpen(() => !open);
+  const handleHamburgerClick = () => {
+    setOpenMobile(() => !openMobile);
+  };
+
+  const handleMenuItemClick = (e) => {
+    if (openSubmenu === e.target.id) {
+      setOpenSubmenu("");
+    } else {
+      setOpenSubmenu(e.target.id);
+    }
+    console.log(openSubmenu);
   };
 
   return (
-    <nav class="relative">
-      <div class="bg-black">
-        <div class="mx-auto max-w-7xl px-4">
-          <div class="relative flex h-20 lg:h-28 items-center justify-between">
-            <div class="flex flex-1 items-center justify-between">
-              <a href="/" class="flex flex-shrink-0 items-center">
+    <nav className="relative">
+      <div className="bg-black">
+        <div className="mx-auto max-w-7xl px-4">
+          <div className="relative flex h-20 lg:h-28 items-center justify-between">
+            <div className="flex flex-1 items-center justify-between">
+              <a href="/" className="flex flex-shrink-0 items-center">
                 <img
                   src="/red-arrow-logo.webp"
                   alt="Red Arrow Logo"
-                  class="h-14 lg:h-20 w-auto"
+                  className="h-14 lg:h-20 w-auto"
                 />
               </a>
-              <div class="hidden lg:ml-6 lg:block">
-                <div class="flex gap-5 xl:gap-10 items-center">
-                  <NavLink object={flightPrograms} />
-                  <NavLink object={resources} />
-                  <NavLink object={about} />
+              <div className="hidden lg:ml-6 lg:block">
+                <div className="flex gap-5 xl:gap-10 items-center">
+                  <NavLink
+                    pathname={pathname}
+                    menuItem={flightPrograms}
+                    toggled={flightPrograms.name === openSubmenu}
+                    onShow={handleMenuItemClick}
+                  />
+                  <NavLink
+                    pathname={pathname}
+                    menuItem={resources}
+                    toggled={resources.name === openSubmenu}
+                    onShow={handleMenuItemClick}
+                  />
+                  <NavLink
+                    pathname={pathname}
+                    menuItem={about}
+                    toggled={about.name === openSubmenu}
+                    onShow={handleMenuItemClick}
+                  />
                   <div>
                     <a
                       href="/discovery-flight"
-                      class="discovery nav-link cursor-pointer text-xl text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap"
+                      className="discovery nav-link cursor-pointer text-xl text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap"
                     >
                       Discovery Flight
                     </a>
                   </div>
-                  <button class="btn-red">BOOK NOW</button>
+                  <button className="btn-red">BOOK NOW</button>
                 </div>
               </div>
             </div>
 
-            <div class="absolute inset-y-0 right-0 flex items-center lg:hidden">
+            <div className="absolute inset-y-0 right-0 flex items-center lg:hidden">
               <button
                 type="button"
-                class="mobile-menu-button relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                className="mobile-menu-button relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
-                onClick={handleClick}
+                onClick={handleHamburgerClick}
               >
-                <span class="absolute -inset-0.5"></span>
-                <span class="sr-only">Open main menu</span>
+                <span className="absolute -inset-0.5"></span>
+                <span className="sr-only">Open main menu</span>
 
                 <svg
-                  class="block h-6 w-6"
+                  className="block h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   aria-hidden="true"
                   id="x-icon"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                   ></path>
                 </svg>
 
                 <svg
-                  class="hidden h-6 w-6"
+                  className="hidden h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   aria-hidden="true"
                   id="hamburger-icon"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M6 18L18 6M6 6l12 12"
                   ></path>
                 </svg>
@@ -90,18 +115,18 @@ const Navbar = ({ pathname }) => {
       </div>
 
       <div
-        class={`${open ? "max-h-[36rem]" : "max-h-0"} overflow-hidden transition-[max-height] duration-300 ease-in-out lg:hidden absolute w-full bg-black -z-10`}
+        className={`${openMobile ? "max-h-[36rem]" : "max-h-0"} overflow-hidden transition-[max-height] duration-300 ease-in-out lg:hidden absolute w-full bg-black -z-10`}
         id="mobile-menu"
       >
-        <div class="px-4 pb-3 pt-2 flex flex-col">
+        <div className="px-4 pb-3 pt-2 flex flex-col">
           <MobileNavBar pathname={pathname} />
           <a
             href="/discovery-flight"
-            class="discovery nav-link cursor-pointer text-xl text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap"
+            className="discovery nav-link cursor-pointer text-xl text-white py-5 hover:border-b-2 border-main-red whitespace-nowrap"
           >
             Discovery Flight
           </a>
-          <button class="btn-red place-self-center">BOOK NOW</button>
+          <button className="btn-red place-self-center">BOOK NOW</button>
         </div>
       </div>
     </nav>
