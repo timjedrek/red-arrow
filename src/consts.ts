8ac5fa7c2f5627179d3b4d0ac1211c7c6d368f6f
Diego <diegoyon@outlook.com>
2024-01-27 23:16:54 -0600

    refactor code and add constants.astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,15 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Red Arrow Flight Academy | El Paso, TX';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const ADDRESS = '8063 Airport Rd';
-export const ADDRESS2 = '';
-export const ADDRESS_CITY = 'Santa Teresa';
-export const ADDRESS_STATE = 'NM';
-export const ADDRESS_ZIP = '88008';
-export const PHONE_NUMBER = '(575) 201-6160';
-export const EMAIL_ADDRESS = 'info@flyredarrow.com';
+export const SITE_TITLE = "Red Arrow Flight Academy | El Paso, TX";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const ADDRESS = "8063 Airport Rd";
+export const ADDRESS2 = "";
+export const ADDRESS_CITY = "Santa Teresa";
+export const ADDRESS_STATE = "NM";
+export const ADDRESS_ZIP = "88008";
+export const PHONE_NUMBER = "(575) 201-6160";
+export const EMAIL_ADDRESS = "info@flyredarrow.com";
 
 export const flightPrograms = {
   name: "Flight Programs",
@@ -18,26 +18,26 @@ export const flightPrograms = {
   submenu: [
     {
       name: "New to flying?",
-      link: "/flight-programs/new-to-flying"
+      link: "/flight-programs/new-to-flying",
     },
     {
       name: "Training Courses",
-      link: "/flight-programs/training-courses"
+      link: "/flight-programs/training-courses",
     },
     {
       name: "University Program",
-      link: "/flight-programs/university-program"
+      link: "/flight-programs/university-program",
     },
     {
       name: "International Program",
-      link: "/flight-programs/international-program"
+      link: "/flight-programs/international-program",
     },
     {
       name: "Military / Rotor Transition",
-      link: "/flight-programs/military-rotor-transition"
-    }
+      link: "/flight-programs/military-rotor-transition",
+    },
   ],
-}
+};
 
 export const resources = {
   name: "Resources",
@@ -46,18 +46,18 @@ export const resources = {
   submenu: [
     {
       name: "FAA Exam Center",
-      link: "/resources/faa-exam-center"
+      link: "/resources/faa-exam-center",
     },
     {
       name: "Financing",
-      link: "/resources/financing"
+      link: "/resources/financing",
     },
     {
       name: "Maintenance",
-      link: "/resources/maintenance"
-    }
+      link: "/resources/maintenance",
+    },
   ],
-}
+};
 
 export const about = {
   name: "About",
@@ -66,19 +66,19 @@ export const about = {
   submenu: [
     {
       name: "Our Story",
-      link: "/about/our-story"
+      link: "/about/our-story",
     },
     {
       name: "Our Team",
-      link: "/about/our-team"
+      link: "/about/our-team",
     },
     {
       name: "Our Fleet",
-      link: "/about/our-fleet"
+      link: "/about/our-fleet",
     },
     {
       name: "Blog",
-      link: "/about/blog"
-    }
+      link: "/about/blog",
+    },
   ],
-}
+};
