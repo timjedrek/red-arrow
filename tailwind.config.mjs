6f58d48ff415c2bb777f25debf043f01297c0002
Diego <diegoyon@outlook.com>
2024-01-30 23:04:17 -0600

    add university subpage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,6 +14,7 @@ export default {
         "our-team": "url('/img/our-team/banner.webp')",
         "our-fleet": "url('/img/our-fleet/banner.webp')",
         "new-to-flying": "url('/img/new-to-flying/banner.jpg')",
+        "university-program": "url('/img/university-program/banner.webp')",
         "training-courses": "url('/img/cessna-outside-hangar-juarez-mx.webp')",
         "discovery-flight":
           "url('/img/discovery-flight/discovery-flight-banner.webp')",
