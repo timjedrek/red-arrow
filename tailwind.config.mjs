02236987ad9fede417d9a65efbb5059a54cff657
Diego <diegoyon@outlook.com>
2024-02-07 14:43:10 -0600

    change font to urbanist

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -43,7 +43,7 @@ export default {
         wiggle: "wiggle 1s ease-in-out infinite",
       },
       fontFamily: {
-        robotoSlab: ["Roboto Slab", "sans-serif"],
+        urbanist: ["Urbanist", "sans-serif"],
       },
     },
   },
