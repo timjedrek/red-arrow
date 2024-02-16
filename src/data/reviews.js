f9d76bec43778dc3cea5fe287f89befb8ccd9304
Diego <diegoyon@outlook.com>
2024-02-15 22:44:02 -0600

    add reviews to homepage

---
+++ src/data/reviews.js
@@ -0,0 +1,32 @@
+export const reviews = [
+  {
+    id: 1,
+    text: "Had a wonderful experience on my intro flight. The instructor was great. Got to actually fly, will never forget it. Would highly recommend it.",
+    author: "Ximena Rodriguez",
+  },
+  {
+    id: 2,
+    text: "I obtained my private pilot license at red arrow. It is by far the best aviation school in the southwest region. The staff is super knowledgeable and the instructors are superbly educated. I went to many additional schools before finishing here. Red Arrow is the perfect explanation on how a school can become a second home. Love it. Don’t look elsewhere.",
+    author: "DAVID ROJAS MEI",
+  },
+  {
+    id: 3,
+    text: "Red Arrow Flight Academy has been an amazing experience, the staff is extremely helpful, and welcoming. The instructors are very experienced and knowledgeable. There is a structural and comprehensive curriculum, and the service is professional and personalized. I highly recommend this school to anyone pursuing any certificate in aviation!",
+    author: "Danielle Nelson",
+  },
+  {
+    id: 4,
+    text: "Excellent experience from start to finish with this crew. No doubt the best facility, flight instructors, staff, airplane fleet, and training simulators in the area. I got my PPL (Private Pilot License) at this flight school and I couldn't have asked for a better introduction into the world of aviation.",
+    author: "Vanessa Martinez",
+  },
+  {
+    id: 5,
+    text: "Tomas and her staff are proffesionals and  makes you feel comfortable. The school is clean and has what you need.  ighly recomended for first time flyers or if you're trying to make a career of it.",
+    author: "Darisabel Roman",
+  },
+  {
+    id: 6,
+    text: "Top notch place to learn to fly. Started there with zero hours all the way through PPL. When and if I ever come back to El Paso, this is the only place I’ll go for flight training.",
+    author: "stoute64",
+  },
+];
