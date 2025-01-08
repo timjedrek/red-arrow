1ef85ab87355127e5924050e81a30633d3070a86
Diego Yon <diegoyon@outlook.com>
2025-01-07 21:42:14 -0600

    remove addAdmin

--- functions/index.js
+++ functions/index.js
@@ -18,28 +18,28 @@ const logger = require("firebase-functions/logger");
 //   response.send("Hello from Firebase!");
 // });
 
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
+// const functions = require("firebase-functions");
+// const admin = require("firebase-admin");
+// admin.initializeApp();
 
-exports.addAdminRole = functions.https.onCall((data, context) => {
-  // check request is made by an admin
-  if (context.auth.token.admin !== true) {
-    return { error: "Only admins can add other admins." };
-  }
-  // get user and add custom claim (admin)
-  return admin
-    .auth()
-    .getUserByEmail(data.email)
-    .then((user) => {
-      return admin.auth().setCustomUserClaims(user.uid, {
-        admin: true,
-      });
-    })
-    .then(() => {
-      return {
-        message: `Success! ${data.email} has been made an admin`,
-      };
-    })
-    .catch((err) => err);
-});
+// exports.addAdminRole = functions.https.onCall((data, context) => {
+//   // check request is made by an admin
+//   if (context.auth.token.admin !== true) {
+//     return { error: "Only admins can add other admins." };
+//   }
+//   // get user and add custom claim (admin)
+//   return admin
+//     .auth()
+//     .getUserByEmail(data.email)
+//     .then((user) => {
+//       return admin.auth().setCustomUserClaims(user.uid, {
+//         admin: true,
+//       });
+//     })
+//     .then(() => {
+//       return {
+//         message: `Success! ${data.email} has been made an admin`,
+//       };
+//     })
+//     .catch((err) => err);
+// });
